6848b28dbc388cca369f36a5aca61bd5b2883a70
skiprez <hubertkaftan@icloud.com>
2024-09-29 14:45:46 +0200

    Update route.js

--- src/app/api/Posts/route.js
+++ src/app/api/Posts/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from 'next/server';
-import { Pool } from 'pg';
+import { Client } from 'pg';
 
-const pool = new Pool({
+const client = new Client({
     connectionString: process.env.POSTGRES_URL,
 });
+  
+client.connect();
 
 export async function GET() {
     const res = await client.query('SELECT * FROM messages ORDER BY created_at ASC');
